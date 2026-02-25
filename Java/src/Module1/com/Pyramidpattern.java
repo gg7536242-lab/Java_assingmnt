7c40c36cd5d3444811fe323c7d83fd399d959509
ganesh <gg7536242@gmail.com>
2026-02-25 21:10:10 +0530

    programs

---
+++ Java/src/Module1/com/Pyramidpattern.java
@@ -0,0 +1,19 @@
+package Module1.com;
+import java.util.Scanner;
+public class Pyramidpattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = rows; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
