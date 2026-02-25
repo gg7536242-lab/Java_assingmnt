fd2813caf45e22121697d70dc0f47f196c803458
ganesh <gg7536242@gmail.com>
2026-02-25 21:16:57 +0530

    programs

---
+++ Java/src/Module2/com/palindrome.java
@@ -0,0 +1,24 @@
+package Module2.com;
+import java.util.Scanner;
+public class palindrome {
+    static boolean isPalindrome(String str) {
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        return str.equalsIgnoreCase(reversed);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        if (isPalindrome(str))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not a palindrome");
+    }
+}
